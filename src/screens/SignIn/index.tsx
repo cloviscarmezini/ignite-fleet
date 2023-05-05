45b84b228ec60b79d0e0871418107d3630bcf279
Clovis Carmezini <cloviscarmezini14@gmail.com>
2023-05-04 23:44:47 -0300

    feat: implement google login

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import backgroundImage from '../../assets/background.png';
 
@@ -10,7 +10,43 @@ import {
 
 import { Button } from '../../components/Button';
 
-export function SignIn(){
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+
+import {
+  ANDROID_CLIENT_ID,
+  IOS_CLIENT_ID
+} from '@env'
+
+WebBrowser.maybeCompleteAuthSession();
+
+export function SignIn() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  const [ _, response, googleSignIn ] = Google.useIdTokenAuthRequest({
+    androidClientId: ANDROID_CLIENT_ID,
+    iosClientId: IOS_CLIENT_ID,
+    scopes: ['profile', 'email']
+  });
+
+  function handleGoogleSignIn() {
+    setIsAuthenticating(true);
+
+    googleSignIn();
+  }
+
+  useEffect(() => {
+    if(response?.type === 'success') {
+      if(response.authentication?.idToken) {
+        fetch(`https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${response.authentication?.idToken}`)
+        .then(response=>response.json())
+        .then(response=>console.log(response))
+      }
+    } else {
+      setIsAuthenticating(false);
+    }
+  }, [response])
+
   return (
     <Container source={backgroundImage}>
       <Title>
@@ -23,6 +59,8 @@ export function SignIn(){
 
       <Button
         title='Entrar com Google'
+        onPress={handleGoogleSignIn}
+        loading={isAuthenticating}
       />
     </Container>
   );
